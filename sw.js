4c5a75fb7f6afc778d16d816df837a4f54de1b0d
AtlasPmos <rick.medeiros@trimarkusa.com>
2026-07-09 04:30:16 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME = 'kinta-genesis-mod-3-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => undefined));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html'))));
-});
